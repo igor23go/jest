1f37d5fbf621db213ab11fc0858406cbee27f907
igor23go <Goigorit23@gmail.com>
2024-01-30 16:09:14 +0200

    task1

--- lesson13jest/task1calculator/index.js
+++ lesson13jest/task1calculator/index.js
@@ -1,28 +1,4 @@
-const calc = (expression) => {
-  let [a, operator, b] = expression.split(' ');
-  let result;
-  a = +a;
-  b = +b;
-  switch (operator) {
-    case '+':
-      result = a + b;
-      break;
-    case '-':
-      result = a - b;
-      break;
-    case '*':
-      result = a * b;
-      break;
-    case '/':
-      result = a / b;
-      break;
-  }
-  return result;
-};
-console.log(calc('4 + 2'));
-
 export const getEvenNumbers = (numbers) =>
   numbers.filter((elem) => elem % 2 == 0);
 
 console.log(getEvenNumbers([1, 2, 3, 4]));
-export default (a, b) => a + b;
