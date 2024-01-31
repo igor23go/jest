dee562cda02062599a9a2fa1462526a331cb46a6
igor23go <Goigorit23@gmail.com>
2024-01-31 15:22:27 +0200

    default export instead of name

--- lesson13jest/task3abs/getMinSquaredNumber.js
+++ lesson13jest/task3abs/getMinSquaredNumber.js
@@ -1,12 +1,10 @@
-function getMinSquaredNumber(arr) {
+export default (arr) => {
   if (!Array.isArray(arr) || arr.length === 0) {
     return null;
   }
-  const absArray = arr.map((elem) => Math.abs(elem));
-  const minNumber = Math.min(...absArray);
-  const number = Math.trunc(minNumber);
-  return Math.pow(number, 2);
-}
-console.log(getMinSquaredNumber([]));
-console.log(getMinSquaredNumber(2));
-console.log(getMinSquaredNumber([2.5, 3, -5, 7, 10.5]));
+  const absValues = arr.map((num) => num * num);
+  return Math.min(...absValues);
+};
+// console.log(getMinSquaredNumber([]));
+// console.log(getMinSquaredNumber(2));
+// console.log(getMinSquaredNumber([2.5, 3, -5, 7, 10.5]));
--- lesson13jest/task3abs/getMinSquaredNumber.test.js
+++ lesson13jest/task3abs/getMinSquaredNumber.test.js
@@ -1,13 +1,13 @@
-import { getMinSquaredNumber } from './getMinSquaredNumber';
+import getMinSquaredNumber from './getMinSquaredNumber';
 it('control if the array is"t empty', () => {
   const result = getMinSquaredNumber([]);
-  expect(result).toBe(null);
+  expect(result).toEqual(null);
 });
 it('control the input data is array data', () => {
   const result = getMinSquaredNumber('abc');
-  expect(result).toBe(null);
+  expect(result).toEqual(null);
 });
 it('control returning squared number', () => {
-  const result = getMinSquaredNumber([5, -3, 2.5]);
+  const result = getMinSquaredNumber([5, -3, 2]);
   expect(result).toBeCloseTo(4);
 });
